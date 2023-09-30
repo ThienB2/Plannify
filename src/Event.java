c920f541695fdb2d8e301e29951225417c1fa068
Thien <thien.buathong@gmail.com>
2023-09-30 15:22:51 -0700

    added constructor for event class

--- src/Event.java
+++ src/Event.java
@@ -1,8 +1,15 @@
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class Event {
     private String name;
     private String subject;
-    private boolean status;
-    private LocalDateTime date;
+    private boolean isCompleted;
+    private LocalDate deadline;
+
+    public Event(String name, String subject, LocalDate time) {
+        this.name = name;
+        this.subject = subject;
+        this.deadline = time;
+        isCompleted = false;
+    }
 }
